3c4a8cb5bf28103a5cabc4c7ae86e6471ad84006
NikitaFalkon <mikita.falcon@gmail.com>
2021-11-09 03:12:42 +0300

    fix: redact some problems

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,54 +2,40 @@ const fs = require('fs');
 const path = require('path');
 const pathToDist = path.join(__dirname, 'project-dist/bundle.css');
 const fileToWrite = fs.createWriteStream(pathToDist);
-let mass = [];
 
 let myFun = function () {
 
-    const myPath = path.join(__dirname, 'styles');
+  const myPath = path.join(__dirname, 'styles');
 
-    fs.readdir(myPath,  function(err, items) {
+  fs.readdir(myPath,  function(err, items) {
 
-      if (err) {
-        console.log('Error', err.message);
-        return;
-      }
+    if (err) {
+      console.log('Error', err.message);
+      return;
+    }
 
-      for (const item of items) {
+    for (const item of items) {
+      const pathsToCheck = path.join(myPath, item);
 
-        const pathsToCheck = path.join(myPath, item);
 
-        fs.stat(pathsToCheck, (err, stats) => {
-          if (err) {
-            console.log('Error', err.message);
-            return;
-          }
+      fs.stat(pathsToCheck, (err, stats) => {
+        if (err) {
+          console.log('Error', err.message);
+          return;
+        }
 
-          if(!stats.isDirectory() && path.extname(item.name) === '.css') {
-            let  streamFile = fs.createReadStream(pathsToCheck,'utf8');
+        if(stats.isFile()) {
+          let fileCont = path.parse(pathsToCheck);
+          if (fileCont.ext === '.css') {
+            let streamFile = fs.createReadStream(pathsToCheck, 'utf8');
             streamFile.on('data', function (chunk) {
               fileToWrite.write(chunk);
             });
           }
-        });
-      }
-
-      // fs.writeFile(myPath, mass[0], function(error){
-      //   if(error) throw error;
-      // });
-
-      console.log(mass);
-
-    });
-
-    // fs.open('text.txt', 'w', (err) => {
-    //   if(err) throw err;
-    //   console.log('File created');
-    // });
-    //
-    // fs.writeFile(pathing, mass[0], function(error){
-    //   if(error) throw error;
-    // });
+        }
+      });
+    }
+  });
 };
 
 myFun();
